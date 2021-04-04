f763ab1e3bf5e8d6368a3a25bb7781cf771a4132
Dirk Mueller <dirkmuller8@outlook.de>
2021-04-04 17:48:06 +0200

    GetClosest method started, but not ready

--- ArabicToRoman/ArabicToRoman/Program.cs
+++ ArabicToRoman/ArabicToRoman/Program.cs
@@ -3,21 +3,6 @@ using System.Collections.Generic;
 
 namespace ArabicToRoman
 {
-    class GoForIt
-    {
-        //public List<int> GetDigits(int number)
-        //{
-        //    int currentNumber = number;
-        //    List<int> digits = new List<int>();
-        //    while (currentNumber != 0)
-        //    {
-        //        digits.Add(currentNumber % 10);
-        //        currentNumber = currentNumber / 10;
-        //    }
-        //    digits.Reverse();
-        //    return digits;
-        //}
-    }
     public class Program
     {
         const int NUMBER = 986;
@@ -25,36 +10,64 @@ namespace ArabicToRoman
 
         static void Main(string[] args)
         {
-            GoForIt gfi = new GoForIt();
-            //gfi.GetDigits(NUMBER);
-            //List<int> digits = new List<int>();
-            SortedList<int, int> digits = new SortedList<int, int>();
+            SortedList<int, char> sl = new SortedList<int, char>();
+            sl.Add(1, 'I');
+            sl.Add(5, 'V');
+            sl.Add(10, 'X');
+            sl.Add(50, 'L');
+            sl.Add(100, 'C');
+            sl.Add(500, 'D');
+            sl.Add(1000, 'M');
 
+            SortedList<int, int> digits;
+            digits = GetDigits(986);
 
-            foreach (var item in GetDigits(986))
+            foreach (var item in digits)
             {
                 Console.WriteLine(item);
             }
+            GetClosest(digits, sl);
+
+        }
+        public static SortedList<int, int> GetClosest(SortedList<int, int> digits, SortedList<int, char> toCompareWith)
+        {
+            int diff = 10000;
+            int temp;
+            int verdacht = 0;
+            foreach (var item in digits)
+            {
+                for (int i = 0; i < toCompareWith.Count; i++)
+                {
+                    temp = Math.Abs(item.Key - toCompareWith.Keys[i]);
+                    Console.WriteLine("temp: {0}", temp);
+                    if (temp < diff)
+                    {
+                        verdacht = toCompareWith.Keys[i];
+                    }
+                    diff = temp;
+                }
+                Console.WriteLine($"item.Key: {item.Key}, item.value: {item.Value}, verdacht: {verdacht}, {toCompareWith[verdacht]}");
+                Console.ReadLine();
+                item.Value = verdacht;
+            }
+            return digits;
         }
         public static SortedList<int, int> GetDigits(int number)
         {
-            int currentNumber = number;
             int a = number;
-            int b = currentNumber;
-            string currentNumberAsString = currentNumber.ToString();
-            //List<int> digits = new List<int>();
+            string currentNumberAsString = number.ToString();
             SortedList<int, int> digits = new SortedList<int, int>();
 
             int l = currentNumberAsString.Length;
             for (int i = 1; i < l; i++)
             {
-                b = (int)(b/Math.Pow(10,l-i));
-                b = (int)(b * Math.Pow(10, l - i));
-                digits.Add((int)(b),0);
-                b = a - b;
-                a = b;
+                number = (int)(number / Math.Pow(10, l - i));
+                number = (int)(number * Math.Pow(10, l - i));
+                digits.Add((int)(number), 0);
+                number = a - number;
+                a = number;
             }
-            digits.Add((int)(b),0);
+            digits.Add((int)(number), 0);
             return digits;
         }
     }
