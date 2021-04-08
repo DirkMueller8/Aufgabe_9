1640da083813c05fe1d608e38eba69b8d38d0ef4
Dirk Mueller <dirkmuller8@outlook.de>
2021-04-08 10:22:26 +0200

    Raw final

--- ArabicToRoman/ArabicToRoman/Program.cs
+++ ArabicToRoman/ArabicToRoman/Program.cs
@@ -7,22 +7,12 @@ namespace ArabicToRoman
 {
     public class Program
     {
-        const int NUMBER = 986;
-        List<int> decomposedNumbers = new List<int>();
-
         static void Main(string[] args)
         {
-            SortedDictionary<int, char> pairs = new SortedDictionary<int, char>();
-            pairs.Add(1, 'I');
-            pairs.Add(5, 'V');
-            pairs.Add(10, 'X');
-            pairs.Add(50, 'L');
-            pairs.Add(100, 'C');
-            pairs.Add(500, 'D');
-            pairs.Add(1000, 'M');
-
+            StringBuilder buildRoman = new StringBuilder();
             int eingabe;
             SortedDictionary<int, int> digits;
+
             while (true)
             {
                 Console.WriteLine("Gib Nummer: ");
@@ -30,53 +20,39 @@ namespace ArabicToRoman
                 if (eingabe == 0)
                     break;
                 digits = GetDigits(eingabe);
-
-                SortedDictionary<int, int> work;
-                work = GetClosest(digits, pairs);
-                foreach (var item in work)
+                foreach (var item in digits)
                 {
                     Console.WriteLine($"{item.Key}, {item.Value}");
                 }
 
-                StringBuilder buildRoman = new StringBuilder();
-                StringBuilder buildRomanH = new StringBuilder();
-                StringBuilder buildRomanT = new StringBuilder();
-                StringBuilder buildRomanO = new StringBuilder();
                 if (digits.Count == 4)
                 {
-                    buildRoman = ProcessThousands(work);
-                    work.Remove(work.ElementAt(3).Key);
+                    buildRoman = ProcessThousands(digits);
+                    digits.Remove(digits.ElementAt(3).Key);
                 }
-                Console.WriteLine("Thousands:");
-                Console.WriteLine($"buildRoman: {buildRoman}");
-                foreach (var item in work)
+
+                foreach (var item in digits)
                 {
                     Console.WriteLine($"{item.Key}, {item.Value}");
                 }
 
                 if (digits.Count == 3)
                 {
-                    buildRomanH = ProcessHundreds(work, buildRoman);
-                    work.Remove(work.ElementAt(2).Key);
+                    buildRoman = ProcessHundreds(digits, buildRoman);
+                    digits.Remove(digits.ElementAt(2).Key);
                 }
-                Console.WriteLine("Hundreds:");
-                Console.WriteLine($"buildRoman: {buildRomanH}");
 
                 if (digits.Count == 2)
                 {
-                    buildRomanT = ProcessTens(work, buildRoman);
-                    work.Remove(work.ElementAt(1).Key);
-                    Console.WriteLine("Letzter: {0}", work.ElementAt(0).Key);
+                    buildRoman = ProcessTens(digits, buildRoman);
+                    digits.Remove(digits.ElementAt(1).Key);
                 }
-                Console.WriteLine("Tens:");
-                Console.WriteLine($"buildRoman: {buildRomanT}");
 
                 if (digits.Count == 1)
                 {
-                    buildRomanO = ProcessOnes(work, buildRoman);
+                    buildRoman = ProcessOnes(digits, buildRoman);
                 }
-                Console.WriteLine("Ones:");
-                Console.WriteLine($"buildRoman: {buildRomanO}");
+                Console.WriteLine("Final result: {0}", buildRoman);
             }
         }
         public static StringBuilder ProcessThousands(SortedDictionary<int, int> dict)
@@ -85,7 +61,7 @@ namespace ArabicToRoman
             int ratio;
             int i = 3;
 
-            ratio = dict.ElementAt(i).Key / dict.ElementAt(i).Value;
+            ratio = dict.ElementAt(i).Value / 1000;
             switch (ratio)
             {
                 case 1:
@@ -103,7 +79,7 @@ namespace ArabicToRoman
         public static StringBuilder ProcessHundreds(SortedDictionary<int, int> dict, StringBuilder sb)
         {
             int i = 2;
-            int wert = dict.ElementAt(i).Key;
+            int wert = dict.ElementAt(i).Value;
             if (wert >= 100 && wert <= 400)
                 sb.Append("C");
             if (wert >= 200 && wert <= 300)
@@ -128,7 +104,7 @@ namespace ArabicToRoman
         public static StringBuilder ProcessTens(SortedDictionary<int, int> dict, StringBuilder sb)
         {
             int i = 1;
-            int wert = dict.ElementAt(i).Key;
+            int wert = dict.ElementAt(i).Value;
             if (wert >= 10 && wert <= 40)
                 sb.Append("X");
             if (wert >= 20 && wert <= 30)
@@ -153,7 +129,7 @@ namespace ArabicToRoman
         public static StringBuilder ProcessOnes(SortedDictionary<int, int> dict, StringBuilder sb)
         {
             int i = 0;
-            int wert = dict.ElementAt(i).Key;
+            int wert = dict.ElementAt(i).Value;
             if (wert >= 1 && wert <= 4)
                 sb.Append("I");
             if (wert >= 2 && wert <= 3)
@@ -171,34 +147,12 @@ namespace ArabicToRoman
             if (wert == 8)
                 sb.Append("I");
             if (wert == 9)
-                sb.Append("VX");
+                sb.Append("IX");
 
             return sb;
         }
 
-        public static SortedDictionary<int, int> GetClosest(SortedDictionary<int, int> digits, SortedDictionary<int, char> toCompareWith)
-        {
-            int diff = 10000;
-            int temp;
-            int verdacht;
 
-            for (int i = 0; i < digits.Count; i++)
-            {
-                foreach (var item in toCompareWith)
-                {
-                    temp = Math.Abs(item.Key - digits.ElementAt(i).Key);
-                    if (temp < diff)
-                    {
-                        verdacht = item.Key;
-                        //Console.WriteLine($"Verdacht: {verdacht}");
-                        digits[digits.ElementAt(i).Key] = verdacht;
-                    }
-                    diff = temp;
-                }
-                diff = 10000;
-            }
-            return digits;
-        }
         public static SortedDictionary<int, int> GetDigits(int number)
         {
             int a = number;
@@ -210,11 +164,12 @@ namespace ArabicToRoman
             {
                 number = (int)(number / Math.Pow(10, l - i));
                 number = (int)(number * Math.Pow(10, l - i));
-                digits.Add((int)(number), 0);
+                digits.Add(l - i, (int)(number));
+
                 number = a - number;
                 a = number;
             }
-            digits.Add((int)(number), 0);
+            digits.Add(0, (int)(number));
             return digits;
         }
     }
--- ArabicToRoman/UnitTestProject1/UnitTest1.cs
+++ ArabicToRoman/UnitTestProject1/UnitTest1.cs
@@ -13,7 +13,7 @@ namespace UnitTestProject1
         {
             SortedDictionary<int, int> digits = new SortedDictionary<int, int>();
 
-            digits.Add(1, 0);
+            digits.Add(0, 1);
 
             for (int i = 0; i < digits.Count; i++)
             {
@@ -25,9 +25,9 @@ namespace UnitTestProject1
         {
             SortedDictionary<int, int> digits = new SortedDictionary<int, int>();
 
-            digits.Add(900, 0);
-            digits.Add(80, 0);
-            digits.Add(6, 0);
+            digits.Add(2, 900);
+            digits.Add(1,80);
+            digits.Add(0,6);
 
             for (int i = 0; i < digits.Count; i++)
             {
@@ -35,14 +35,28 @@ namespace UnitTestProject1
             }
         }
         [TestMethod]
+        public void Number906gives900_6()
+        {
+            SortedDictionary<int, int> digits = new SortedDictionary<int, int>();
+
+            digits.Add(2, 900);
+            digits.Add(1, 0);
+            digits.Add(0, 6);
+
+            for (int i = 0; i < digits.Count; i++)
+            {
+                Assert.AreEqual(digits.ElementAt(i), Program.GetDigits(906).ElementAt(i));
+            }
+        }
+        [TestMethod]
         public void Number3962gives3000_900_60_2()
         {
             SortedDictionary<int, int> digits = new SortedDictionary<int, int>();
 
-            digits.Add(3000, 0);
-            digits.Add(900, 0);
-            digits.Add(60, 0);
-            digits.Add(2, 0);
+            digits.Add(3, 3000);
+            digits.Add(2,900);
+            digits.Add(1,60);
+            digits.Add(0, 2);
 
             for (int i = 0; i < digits.Count; i++)
             {
@@ -50,86 +64,33 @@ namespace UnitTestProject1
             }
         }
         [TestMethod]
-        public void Number3962gives3000_1000_900__900_1000__60_50__2_1()
+        public void Number3069gives3000_0_60_9()
         {
-            SortedDictionary<int, char> pairs = new SortedDictionary<int, char>();
-            pairs.Add(1, 'I');
-            pairs.Add(5, 'V');
-            pairs.Add(10, 'X');
-            pairs.Add(50, 'L');
-            pairs.Add(100, 'C');
-            pairs.Add(500, 'D');
-            pairs.Add(1000, 'M');
-            
             SortedDictionary<int, int> digits = new SortedDictionary<int, int>();
-            digits.Add(3000, 0);
-            digits.Add(900, 0);
-            digits.Add(60, 0);
-            digits.Add(2, 0);
 
-            SortedDictionary<int, int> expectedResult = new SortedDictionary<int, int>();
-            expectedResult.Add(3000, 1000);
-            expectedResult.Add(900, 1000);
-            expectedResult.Add(60, 50);
-            expectedResult.Add(2, 1);
+            digits.Add(3, 3000);
+            digits.Add(2, 0);
+            digits.Add(1, 60);
+            digits.Add(0, 9);
 
             for (int i = 0; i < digits.Count; i++)
             {
-                Assert.AreEqual(expectedResult.ElementAt(i), Program.GetClosest(digits, pairs).ElementAt(i));
+                Assert.AreEqual(digits.ElementAt(i), Program.GetDigits(3069).ElementAt(i));
             }
         }
         [TestMethod]
-        public void Number3752gives3000_1000_700__500_50_50__2_1()
+        public void Number3000gives3000_0_0_0()
         {
-            SortedDictionary<int, char> pairs = new SortedDictionary<int, char>();
-            pairs.Add(1, 'I');
-            pairs.Add(5, 'V');
-            pairs.Add(10, 'X');
-            pairs.Add(50, 'L');
-            pairs.Add(100, 'C');
-            pairs.Add(500, 'D');
-            pairs.Add(1000, 'M');
-
             SortedDictionary<int, int> digits = new SortedDictionary<int, int>();
-            digits.Add(3000, 0);
-            digits.Add(700, 0);
-            digits.Add(50, 0);
-            digits.Add(2, 0);
 
-            SortedDictionary<int, int> expectedResult = new SortedDictionary<int, int>();
-            expectedResult.Add(3000, 1000);
-            expectedResult.Add(700, 500);
-            expectedResult.Add(50, 50);
-            expectedResult.Add(2, 1);
+            digits.Add(3, 3000);
+            digits.Add(2, 0);
+            digits.Add(1, 0);
+            digits.Add(0, 0);
 
             for (int i = 0; i < digits.Count; i++)
             {
-                Assert.AreEqual(expectedResult.ElementAt(i), Program.GetClosest(digits, pairs).ElementAt(i));
-            }
-        }
-        [TestMethod]
-        public void Number12gives10__10__2_1()
-        {
-            SortedDictionary<int, char> pairs = new SortedDictionary<int, char>();
-            pairs.Add(1, 'I');
-            pairs.Add(5, 'V');
-            pairs.Add(10, 'X');
-            pairs.Add(50, 'L');
-            pairs.Add(100, 'C');
-            pairs.Add(500, 'D');
-            pairs.Add(1000, 'M');
-
-            SortedDictionary<int, int> digits = new SortedDictionary<int, int>();
-            digits.Add(10, 0);
-            digits.Add(2, 0);
-
-            SortedDictionary<int, int> expectedResult = new SortedDictionary<int, int>();
-            expectedResult.Add(10, 10);
-            expectedResult.Add(2, 1);
-
-            for (int i = 0; i < digits.Count; i++)
-            {
-                Assert.AreEqual(expectedResult.ElementAt(i), Program.GetClosest(digits, pairs).ElementAt(i));
+                Assert.AreEqual(digits.ElementAt(i), Program.GetDigits(3000).ElementAt(i));
             }
         }
     }
